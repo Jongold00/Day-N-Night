5e07103ccebce411b0c4a5213ccdecccd1992351
Losted <pynjunior@gmail.com>
2023-03-26 18:00:52 +0100

    initial commit

---
+++ Source/DayNnight/DayNnight.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DayNnight.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DayNnight, "DayNnight" );
---
+++ Source/DayNnight/DayNnightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DayNnightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAYNNIGHT_API ADayNnightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
